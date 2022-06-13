539d6619c2459c221b124dd0002e593d4cc65877
Amanbek Faizolla <88109946+Rahugg@users.noreply.github.com>
2022-06-14 01:29:15 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import './App.css';
+import { FaBeer } from "react-icons/fa";
+function App() {
+  return (
+    
+    <div className='images'>
+      <header className='headerr'>
+        <h1>In proccess</h1>
+      </header>
+      <div className='Amanbek'>
+        <div className='photos'>
+        <img src = "https://source.unsplash.com/gYl-UtwNg_I/1500x1500"/>
+        <h1 className='txt'>HEY!</h1>
+      </div>
+      <div className='photos'>
+        <img src = "https://source.unsplash.com/rFKUFzjPYiQ/1500x1500"/>
+        <h1 className='txt'>LET'S</h1>
+      </div>
+      <div className='photos'>
+        <img src = "https://images.unsplash.com/photo-1465188162913-8fb5709d6d57?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&w=1500&h=1500&fit=crop&s=967e8a713a4e395260793fc8c802901d"/>
+        <h1 className='txt'>GIVE</h1>
+        </div>
+      <div className='photos'>
+        <img src = "https://source.unsplash.com/ITjiVXcwVng/1500x1500"/>   
+        <h1 className='txt'>ALL</h1></div>
+      <div className='photos'>
+        <img src = "https://source.unsplash.com/3MNzGlQM7qs/1500x1500"/>
+        <h1 className='txt'>YOU CAN</h1>
+      </div>
+      </div>
+    </div>
+    
+
+  );
+}
+
+export default App;
